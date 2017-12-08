00612f562780d7c70b7b8fb81dd1a067137e1dbd
Simon Crowther <simon.crowther95@hotmail.co.uk>
2017-12-08 09:25:50 +0000

    Update to design HL

--- System Project/first_phase/first_phase.adb
+++ System Project/first_phase/first_phase.adb
@@ -54,5 +54,34 @@ package body First_Phase is
    
    procedure Initialise is separate;
    
+      --Child Id Array
+   type Child_Test_Loadout_Index_Type is range 1..19;
+
+   type Child_Test_Loadout_Array_Type is array (Child_Test_Loadout_Index_Type) of Failure_And_Event_Types.Fault_Id_Type;
+
+   type Loadout_Test_Index_Type is range 1 ..5;
+
+   type Loadout_Test_Array_Type is array (Loadout_Test_Index_Type) of Loadout_Type;
+
+   type Loadout_Test_Attributes_Type is record 
+      Fault_Id        : Failure_And_Event_Types.Fault_Id_Type;
+      Station_Id      : Sms_Types.Station_Index_Subtype;
+      Loadouts        : Loadout_Test_Array_Type;
+      Child_Id        : Child_Test_Loadout_Array_Type; 
+   end record;
+
+   type Loadout_Test_Attributes_Array_Type is array (Fault_Lookup_Loadout_Test_ID_Type) of Loadout_Test_Attributes_Type;
+
+   Loadout_Test_Attributes: constant Loadout_Test_Attributes_Array_Type:=Loadout_Test_Attributes_Array_Type'
+     (Test_1143 => Loadout_Test_Attributes_Type'(
+         Fault_Id => Failure_And_Event_Types.Test_1143,
+         Station_Id => 3,
+         Loadouts => Loadout_Test_Array_Type'(PIE_In_flight_guided_store,
+                                          Others => Placeholder_Loadout),
+         Child_Id => Child_Test_Loadout_Array_Type'(Failure_And_Event_Types.Test_9502,
+                                          Failure_And_Event_Types.Test_9503,
+                                          Failure_And_Event_Types.Test_9504,
+                                          Others => Failure_And_Event_Types.Test_0)),
+
 
 end First_Phase;
