87d2088b37b58c3440104d342895b57eef2b1a5d
Simon Crowther <simon.crowther95@hotmail.co.uk>
2018-05-21 17:46:03 +0100

    Script update

--- Manuals/Scripts/Ada_Code_File_Creation.py
+++ Manuals/Scripts/Ada_Code_File_Creation.py
@@ -13,10 +13,39 @@ from shutil import copyfile
 import fileinput
 #Import of necessary libraries
 
-os.chdir("C:/Users/simon/Documents/Programming/Ada/System Checks/Software-Requirements/BIT")
-Main_Path = ("C:/Users/simon/Documents/Programming/Ada/System Checks/System Project/BIT")
-Template = ("C:/Users/simon/Documents/Programming/Ada/System Checks/System Project/Template Header.adb")
+Main_2 = ("C:/Users/simon/Documents/Programming/Ada/System Checks")
+Main   = ("C:/Users/simon/Documents/Github/Projects/ADLAS_Ada_System")  
 
+Template = (Main + '/' + "System Project/Template Header.adb")
+
+Project_Software_List = [(Main + '/' + "Software-Requirements/BIT"),
+                         (Main + '/' + "Software-Requirements/ADLAS.Initialise_Target"),
+                         (Main + '/' + "Software-Requirements/ADLAS.Phase_One_Analyse")]
+
+Software_Requirements_List = [(Main + '/' + "System Project/BIT"),
+                              (Main + '/' + "System Project/ADLAS.Initialise_Target"),
+                              (Main + '/' + "System Project/ADLAS.Phase_One_Analyse")]
+
+Project_Names = ["BIT",
+                 "ADLAS.Initialise_Target",
+                 "ADLAS.Phase_One_Analyse"]
+
+
+def Project_Selection():
+    print("#--------------------------------------------------------#")
+    print("   Project Selection for ada autogeneration tool         ")
+    print("#--------------------------------------------------------#")
+    Index = 1
+    for Project in Project_Names:
+        print(str(Index) + ": Project Name: ", Project)
+        Index += 1
+    Index =- 1
+    Choice = int(input("Select a project number?: "))
+    print("#--------------------------------------------------------#")
+    print("Project Name: " + Project_Names[Choice])
+    print("#--------------------------------------------------------#")
+    return Choice
+    
 
 def Replace(Ada, Function_Name):
     with open(Ada, "r") as File:
@@ -38,8 +67,8 @@ def Package_Body_A_Spec(File_Name_Lower_1, File_Name_Lower_2):
             for line in f:
                 File.write(line)
         File.close()
-        Replace(Package_Body, none)
         print('Creating Package Body')
+    Replace(Package_Body, none)    
     if (os.path.isfile(Package_Spec)) == False:
         File = open((Package_Spec), "w")
         with open(Template, 'r') as f:
@@ -49,19 +78,22 @@ def Package_Body_A_Spec(File_Name_Lower_1, File_Name_Lower_2):
         Replace(Package_Spec, none)
         print('Creating Package Spec')
 
-def Workbook():
+def Workbook(Choice):
+    Req_Path = Software_Requirements_List[Choice]
+    Proj_Path = Project_Software_List[Choice]
+    os.chdir(Req_Path)
     Workbook_Name = input("Workbook Name? ")
     Workbook = load_workbook(Workbook_Name + '.xlsx')
     # grab the active worksheet
     Worksheet = Workbook.active
-    os.chdir(Main_Path)
+    os.chdir(Proj_Path)
     Column_Group = int(input("Which column for Group?: "))
     Column_Sub_Group = int(input("Which column for Sub_Group?: "))
     Column_Procedure = int(input("Which column for Procedures?: "))
     # Data can be assigned directly to cells
     Row = 2    
     while Worksheet.cell(row=Row, column=Column_Procedure).value is not None:
-        os.chdir(Main_Path)
+        os.chdir(Proj_Path)
         File_Name_Procedure = Worksheet.cell(row=Row, column=Column_Procedure).value
         File_Name_Sub_Group = Worksheet.cell(row=Row, column=Column_Sub_Group).value
         File_Name_Group = Worksheet.cell(row=Row, column=Column_Group).value
@@ -107,7 +139,8 @@ def Workbook():
     # Save the file
     Workbook.save(Workbook_Name + 'xlsx')
 
-Workbook()
+Choice = Project_Selection()
+Workbook(Choice)
 
 
 
